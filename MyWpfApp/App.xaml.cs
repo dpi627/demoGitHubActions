a5b07e575e0935f9922d1e4f6f868541195e8ff3
brian_li <brian.li@sgs.com>
2024-04-26 14:45:04 +0800

    add a empty wpf project

---
+++ MyWpfApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyWpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
